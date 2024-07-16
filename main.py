5bd577abcf75af385d06e73b1ed281a54e690381
Coin Hunters <111747226+CoinHuntersTR@users.noreply.github.com>
2024-07-16 12:39:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from loguru import logger
 
 # Introduction and user confirmation
 def print_intro():
-echo -e "\033[0;31m"
-echo "  ____      _         _   _             _                 "  
-echo " / ___|___ (_)_ __   | | | |_   _ _ __ | |_ ___ _ __ ___  "
-echo "| |   / _ \| | '_ \  | |_| | | | | '_ \| __/ _ \ '__/ __| " 
-echo "| |__| (_) | | | | | |  _  | |_| | | | | ||  __/ |  \__ \ "
-echo " \____\___/|_|_| |_| |_| |_|\__,_|_| |_|\__\___|_|  |___/ "
-echo -e "\e[0m"
-    answer = input('Will you F** NODEPAY Airdrop? (Y/N): ')
+    echo -e "\033[0;31m"
+    echo "  ____      _         _   _             _                 "  
+    echo " / ___|___ (_)_ __   | | | |_   _ _ __ | |_ ___ _ __ ___  "
+    echo "| |   / _ \| | '_ \  | |_| | | | | '_ \| __/ _ \ '__/ __| " 
+    echo "| |__| (_) | | | | | |  _  | |_| | | | | ||  __/ |  \__ \ "
+    echo " \____\___/|_|_| |_| |_| |_|\__,_|_| |_|\__\___|_|  |___/ "
+    echo -e "\e[0m"
+    answer = input('Will you NODEPAY Airdrop? (Y/N): ')
     if answer.lower() != 'y':
         print('Aborting installation.')
         exit(1)
